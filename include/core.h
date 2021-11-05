9bcda8b692b48e095bc3a2f383cff24ed719e5b0
yumcyawiz <hashdolf1923@gmail.com>
2021-11-05 17:19:33 +0900

    remove _ from member

--- include/core.h
+++ include/core.h
@@ -3,61 +3,61 @@
 #include <limits>
 
 struct Vec3 {
-  float x_;
-  float y_;
-  float z_;
+  float x;
+  float y;
+  float z;
 
-  Vec3() { x_ = y_ = z_ = 0; }
-  Vec3(float x) { x_ = y_ = z_ = x; }
-  Vec3(float x, float y, float z) : x_(x), y_(y_), z_(z_) {}
+  Vec3() { x = y = z = 0; }
+  Vec3(float x) { x = y = z = x; }
+  Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
 };
 
 inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
-  return Vec3(v1.x_ + v2.x_, v1.y_ + v2.y_, v1.z_ + v2.z_);
+  return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 inline Vec3 operator+(const Vec3& v1, float k) {
-  return Vec3(v1.x_ + k, v1.y_ + k, v1.z_ + k);
+  return Vec3(v1.x + k, v1.y + k, v1.z + k);
 }
 inline Vec3 operator+(float k, const Vec3& v2) { return v2 + k; }
 
 inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
-  return Vec3(v1.x_ - v2.x_, v1.y_ - v2.y_, v1.z_ - v2.z_);
+  return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
 }
 inline Vec3 operator-(const Vec3& v1, float k) {
-  return Vec3(v1.x_ - k, v1.y_ - k, v1.z_ - k);
+  return Vec3(v1.x - k, v1.y - k, v1.z - k);
 }
 inline Vec3 operator-(float k, const Vec3& v2) {
-  return Vec3(k - v2.x_, k - v2.y_, k - v2.z_);
+  return Vec3(k - v2.x, k - v2.y, k - v2.z);
 }
 
 inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
-  return Vec3(v1.x_ * v2.x_, v1.y_ * v2.y_, v1.z_ * v2.z_);
+  return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
 }
 inline Vec3 operator*(const Vec3& v1, float k) {
-  return Vec3(v1.x_ * k, v1.y_ * k, v1.z_ * k);
+  return Vec3(v1.x * k, v1.y * k, v1.z * k);
 }
 inline Vec3 operator*(float k, const Vec3& v2) { return v2 * k; }
 
 inline Vec3 operator/(const Vec3& v1, const Vec3& v2) {
-  return Vec3(v1.x_ / v2.x_, v1.y_ / v2.y_, v1.z_ / v2.z_);
+  return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
 }
 inline Vec3 operator/(const Vec3& v1, float k) {
-  return Vec3(v1.x_ / k, v1.y_ / k, v1.z_ / k);
+  return Vec3(v1.x / k, v1.y / k, v1.z / k);
 }
 inline Vec3 operator/(float k, const Vec3& v2) {
-  return Vec3(k / v2.x_, k / v2.y_, k / v2.z_);
+  return Vec3(k / v2.x, k / v2.y, k / v2.z);
 }
 
 struct Ray {
-  Vec3 origin_;
-  Vec3 direction_;
+  Vec3 origin;
+  Vec3 direction;
   static constexpr float tmin = 1e-3;
   static constexpr float tmax = std::numeric_limits<float>::max();
 
   Ray(const Vec3& origin, const Vec3& direction)
-      : origin_(origin), direction_(direction) {}
+      : origin(origin), direction(direction) {}
 
-  Vec3 operator()(float t) const { return origin_ + t * direction_; }
+  Vec3 operator()(float t) const { return origin + t * direction; }
 };
 
 struct IntersectInfo {
