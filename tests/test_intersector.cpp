621c5d3ac929f8e4011cdc6f1bf60e5711104d38
yumcyawiz <hashdolf1923@gmail.com>
2021-11-08 19:55:47 +0900

    add test_intersector.cpp

--- tests/test_intersector.cpp
+++ tests/test_intersector.cpp
@@ -1,6 +1,5 @@
-#include "core.h"
+#include "camera.h"
 #include "image.h"
-#include "material.h"
 #include "primitive.h"
 #include "scene.h"
 
@@ -8,20 +7,42 @@ int main() {
   const int width = 512;
   const int height = 512;
 
+  const auto sphere_shape = std::make_shared<Sphere>(Vec3(0, 1, 0), 1.0f);
+  const auto sphere2_shape = std::make_shared<Sphere>(Vec3(-1, 1, -1), 1.0f);
+  const auto sphere3_shape = std::make_shared<Sphere>(Vec3(1, 1, 1), 1.0f);
   const auto floor_shape =
-      std::make_shared<Plane>(Vec3(-5), Vec3(5, 0, 0), Vec3(0, 0, 5));
+      std::make_shared<Plane>(Vec3(5, 0, -5), Vec3(-10, 0, 0), Vec3(0, 0, 10));
 
   const Primitive floor(floor_shape, nullptr);
+  const Primitive sphere(sphere_shape, nullptr);
+  const Primitive sphere2(sphere2_shape, nullptr);
+  const Primitive sphere3(sphere3_shape, nullptr);
 
   Scene scene;
   scene.addPrimitive(floor);
+  scene.addPrimitive(sphere);
+  scene.addPrimitive(sphere2);
+  scene.addPrimitive(sphere3);
+  scene.build();
+
+  Camera camera(Vec3(0, 1, 5), Vec3(0, 0, -1));
 
   Image image(width, height);
   for (int i = 0; i < height; ++i) {
-    const float u = static_cast<float>(i) / height;
     for (int j = 0; j < width; ++j) {
-      const float v = static_cast<float>(j) / height;
-      image.setPixel(i, j, Vec3(u, v, 1.0f));
+      const float u = (2.0f * j - width) / height;
+      const float v = (2.0f * i - height) / height;
+
+      Ray ray;
+      float pdf;
+      if (camera.sampleRay(u, v, ray, pdf)) {
+        IntersectInfo info;
+        if (scene.intersect(ray, info)) {
+          image.setPixel(i, j, 0.5f * (info.hitNormal + 1.0f));
+        }
+      } else {
+        image.setPixel(i, j, Vec3(0));
+      }
     }
   }
 
