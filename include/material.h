7395c7bce90962aead38d7e4bcba28c38fa2f5c7
yumcyawiz <hashdolf1923@gmail.com>
2021-11-10 01:25:01 +0900

    impl Diffuse sample BRDF

--- include/material.h
+++ include/material.h
@@ -1,9 +1,12 @@
 #ifndef _MATERIAL_H
 #define _MATERIAL_H
 #include "core.h"
+#include "sampler.h"
 
 class Material {
-  virtual Vec3 sampleBRDF(const Vec3& wo, Vec3& wi, float& pdf) const;
+  virtual Vec3 BRDF(const Vec3& wo, const Vec3& wi) const = 0;
+  virtual Vec3 sampleBRDF(const Vec3& wo, Sampler& sampler, Vec3& wi,
+                          float& pdf) const;
 };
 
 class Diffuse : public Material {
@@ -11,6 +14,15 @@ class Diffuse : public Material {
   Vec3 rho;
 
   Diffuse(const Vec3& rho) : rho(rho) {}
+
+  Vec3 BRDF(const Vec3& wo, const Vec3& wi) const { return rho / PI; }
+
+  Vec3 sampleBRDF(const Vec3& wo, Sampler& sampler, Vec3& wi,
+                  float& pdf) const override {
+    // cosine weighted hemisphere sampling
+    wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+    return BRDF(wo, wi);
+  }
 };
 
 class Mirror : public Material {};
