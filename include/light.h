2782ba0001ea5d946d988a2ba356cefaf8745e9e
yumcyawiz <hashdolf1923@gmail.com>
2021-11-13 18:03:17 +0900

    add const on Light::Le

--- include/light.h
+++ include/light.h
@@ -8,7 +8,7 @@
 
 class Light {
  public:
-  virtual Vec3 Le(const SurfaceInfo& info, Vec3& dir) const = 0;
+  virtual Vec3 Le(const SurfaceInfo& info, const Vec3& dir) const = 0;
   virtual SurfaceInfo samplePoint(Sampler& sampler, float& pdf) const = 0;
   virtual Vec3 sampleDirection(const SurfaceInfo& surfInfo, Sampler& sampler,
                                float& pdf) const = 0;
@@ -23,7 +23,9 @@ class AreaLight : public Light {
   AreaLight(const Vec3& le, const std::shared_ptr<Shape>& shape)
       : le(le), shape(shape) {}
 
-  Vec3 Le(const SurfaceInfo& info, Vec3& dir) const { return le; }
+  Vec3 Le(const SurfaceInfo& info, const Vec3& dir) const override {
+    return le;
+  }
 
   SurfaceInfo samplePoint(Sampler& sampler, float& pdf) const override {
     return shape->samplePoint(sampler, pdf);
