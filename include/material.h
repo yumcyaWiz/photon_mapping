c110c9e725c661a21457a2220a341e1f4aa3878f
yumcyawiz <hashdolf1923@gmail.com>
2021-11-13 17:22:01 +0900

    add material type on sampleBRDF

--- include/material.h
+++ include/material.h
@@ -3,11 +3,13 @@
 #include "core.h"
 #include "sampler.h"
 
+enum class MaterialType { DIFFUSE, SPECULAR };
+
 class Material {
  public:
   virtual Vec3 BRDF(const Vec3& wo, const Vec3& wi) const = 0;
   virtual Vec3 sampleBRDF(const Vec3& wo, Sampler& sampler, Vec3& wi,
-                          float& pdf) const;
+                          MaterialType& type, float& pdf) const;
 };
 
 class Diffuse : public Material {
@@ -20,9 +22,12 @@ class Diffuse : public Material {
   Vec3 BRDF(const Vec3& wo, const Vec3& wi) const { return rho / PI; }
 
   Vec3 sampleBRDF(const Vec3& wo, Sampler& sampler, Vec3& wi,
-                  float& pdf) const override {
+                  MaterialType& type, float& pdf) const override {
     // cosine weighted hemisphere sampling
     wi = sampleCosineHemisphere(sampler.getNext2D(), pdf);
+
+    type = MaterialType::DIFFUSE;
+
     return BRDF(wo, wi);
   }
 };
