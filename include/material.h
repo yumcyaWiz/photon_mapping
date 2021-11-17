c7891d2b9ec2b795733d29faeb21aa2b47af7b72
yumcyawiz <hashdolf1923@gmail.com>
2021-11-18 05:49:30 +0900

    impl glass bxdf

--- include/material.h
+++ include/material.h
@@ -15,9 +15,29 @@ class BxDF {
   BxDF(const BxDFType& type) : type(type) {}
 
   static float cosTheta(const Vec3& v) { return v[1]; }
+  static float absCosTheta(const Vec3& v) { return std::abs(cosTheta(v)); }
 
   static Vec3 reflect(const Vec3& v, const Vec3& n) {
     return -v + 2.0f * dot(v, n) * n;
+  }
+  static bool refract(const Vec3& v, const Vec3& n, float iorI, float iorT,
+                      Vec3& t) {
+    const Vec3 t_h = -iorI / iorT * (v - dot(v, n) * n);
+    // total reflection
+    if (length2(t_h) > 1.0f) {
+      return false;
+    }
+    const Vec3 t_p = -std::sqrt(std::max(1.0f - length2(t_h), 0.0f)) * n;
+    t = t_h + t_p;
+    return true;
+  }
+
+  // schlick approximation
+  static float fresnel(float cosThetaI, float iorI, float iorT) {
+    const float f0 =
+        (iorI - iorT) * (iorI - iorT) / ((iorI + iorT) * (iorI + iorT));
+    const auto pow5 = [](float x) { return x * x * x * x * x; };
+    return f0 + (1.0f - f0) * pow5(1.0f - std::abs(cosThetaI));
   }
 
   BxDFType getType() const { return type; }
@@ -36,8 +56,8 @@ class Lambert : public BxDF {
 
   Vec3 evaluate(const Vec3& wo, const Vec3& wi) const override {
     // when wo, wi is under the surface, return 0
-    const float cosThetaO = BxDF::cosTheta(wo);
-    const float cosThetaI = BxDF::cosTheta(wi);
+    const float cosThetaO = cosTheta(wo);
+    const float cosThetaI = cosTheta(wi);
     if (cosThetaO < 0 || cosThetaI < 0) return Vec3(0);
 
     return rho / PI;
@@ -70,6 +90,62 @@ class Mirror : public BxDF {
     pdf = 1.0f;
 
     return rho;
+  }
+};
+
+class Glass : public BxDF {
+ private:
+  Vec3 rho;
+  float ior;
+
+ public:
+  Glass(const Vec3& rho, float ior)
+      : BxDF(BxDFType::SPECULAR), rho(rho), ior(ior) {}
+
+  // NOTE: delta function
+  Vec3 evaluate(const Vec3& wo, const Vec3& wi) const override {
+    return Vec3(0);
+  }
+
+  Vec3 sampleDirection(const Vec3& wo, Sampler& sampler, Vec3& wi,
+                       float& pdf) const override {
+    // set appropriate ior, normal
+    float iorI, iorT;
+    Vec3 n;
+    if (wo[1] > 0) {
+      iorI = 1.0f;
+      iorT = ior;
+      n = Vec3(0, 1, 0);
+    } else {
+      iorI = ior;
+      iorT = 1.0f;
+      n = Vec3(0, -1, 0);
+    }
+
+    // fresnel reflectance
+    const float fr = fresnel(dot(wo, n), iorI, iorT);
+
+    // reflection
+    if (sampler.getNext1D() < fr) {
+      wi = reflect(wo, n);
+      pdf = 1.0f;
+      return rho / absCosTheta(wi);
+    }
+    // refraction
+    else {
+      Vec3 tr;
+      if (refract(wo, n, iorI, iorT, tr)) {
+        wi = tr;
+        pdf = 1.0f;
+        return rho / absCosTheta(wi);
+      }
+      // 全反射の場合
+      else {
+        wi = reflect(wo, n);
+        pdf = 1.0f;
+        return rho / absCosTheta(wi);
+      }
+    }
   }
 };
 
