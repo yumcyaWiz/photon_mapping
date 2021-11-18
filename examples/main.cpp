b75bb412e9050d0e8f31e1ef9d99f7b8e54c1f14
yumcyawiz <hashdolf1923@gmail.com>
2021-11-18 15:54:00 +0900

    test glass scene

--- examples/main.cpp
+++ examples/main.cpp
@@ -105,13 +105,74 @@ void cornellboxMirrorScene(Scene& scene) {
   scene.addPrimitive(Primitive(light_shape, white, light));
 }
 
+void cornellboxGlassScene(Scene& scene) {
+  const auto white = std::make_shared<Lambert>(Vec3(0.8));
+  const auto red = std::make_shared<Lambert>(Vec3(0.8, 0.05, 0.05));
+  const auto green = std::make_shared<Lambert>(Vec3(0.05, 0.8, 0.05));
+  const auto glass = std::make_shared<Glass>(Vec3(0.99), 1.5f);
+
+  const auto floor =
+      std::make_shared<Plane>(Vec3(0), Vec3(0, 0, 5.592), Vec3(5.56, 0, 0));
+  const auto rightWall =
+      std::make_shared<Plane>(Vec3(0), Vec3(0, 5.488, 0), Vec3(0, 0, 5.592));
+  const auto leftWall = std::make_shared<Plane>(
+      Vec3(5.56, 0, 0), Vec3(0, 0, 5.592), Vec3(0, 5.488, 0));
+  const auto ceil = std::make_shared<Plane>(Vec3(0, 5.488, 0), Vec3(5.56, 0, 0),
+                                            Vec3(0, 0, 5.592));
+  const auto backWall = std::make_shared<Plane>(
+      Vec3(0, 0, 5.592), Vec3(0, 5.488, 0), Vec3(5.56, 0, 0));
+
+  const auto shortBox1 = std::make_shared<Plane>(
+      Vec3(1.3, 1.65, 0.65), Vec3(-0.48, 0, 1.6), Vec3(1.6, 0, 0.49));
+  const auto shortBox2 = std::make_shared<Plane>(
+      Vec3(2.9, 0, 1.14), Vec3(0, 1.65, 0), Vec3(-0.5, 0, 1.58));
+  const auto shortBox3 = std::make_shared<Plane>(
+      Vec3(1.3, 0, 0.65), Vec3(0, 1.65, 0), Vec3(1.6, 0, 0.49));
+  const auto shortBox4 = std::make_shared<Plane>(
+      Vec3(0.82, 0, 2.25), Vec3(0, 1.65, 0), Vec3(0.48, 0, -1.6));
+  const auto shortBox5 = std::make_shared<Plane>(
+      Vec3(2.4, 0, 2.72), Vec3(0, 1.65, 0), Vec3(-1.58, 0, -0.47));
+
+  const auto tallBox1 = std::make_shared<Plane>(
+      Vec3(4.23, 3.30, 2.47), Vec3(-1.58, 0, 0.49), Vec3(0.49, 0, 1.59));
+  const auto tallBox2 = std::make_shared<Plane>(
+      Vec3(4.23, 0, 2.47), Vec3(0, 3.3, 0), Vec3(0.49, 0, 1.59));
+  const auto tallBox3 = std::make_shared<Plane>(
+      Vec3(4.72, 0, 4.06), Vec3(0, 3.3, 0), Vec3(-1.58, 0, 0.5));
+  const auto tallBox4 = std::make_shared<Plane>(
+      Vec3(3.14, 0, 4.56), Vec3(0, 3.3, 0), Vec3(-0.49, 0, -1.6));
+  const auto tallBox5 = std::make_shared<Plane>(
+      Vec3(2.65, 0, 2.96), Vec3(0, 3.3, 0), Vec3(1.58, 0, -0.49));
+
+  const auto light_shape = std::make_shared<Plane>(
+      Vec3(3.43, 5.486, 2.27), Vec3(0, 0, 1.05), Vec3(-1.3, 0, 0));
+  const auto light = std::make_shared<AreaLight>(Vec3(34, 19, 10), light_shape);
+
+  scene.addPrimitive(Primitive(floor, white));
+  scene.addPrimitive(Primitive(rightWall, red));
+  scene.addPrimitive(Primitive(leftWall, green));
+  scene.addPrimitive(Primitive(ceil, white));
+  scene.addPrimitive(Primitive(backWall, white));
+  scene.addPrimitive(Primitive(shortBox1, glass));
+  scene.addPrimitive(Primitive(shortBox2, glass));
+  scene.addPrimitive(Primitive(shortBox3, glass));
+  scene.addPrimitive(Primitive(shortBox4, glass));
+  scene.addPrimitive(Primitive(shortBox5, glass));
+  scene.addPrimitive(Primitive(tallBox1, glass));
+  scene.addPrimitive(Primitive(tallBox2, glass));
+  scene.addPrimitive(Primitive(tallBox3, glass));
+  scene.addPrimitive(Primitive(tallBox4, glass));
+  scene.addPrimitive(Primitive(tallBox5, glass));
+  scene.addPrimitive(Primitive(light_shape, white, light));
+}
+
 int main() {
   const int width = 512;
   const int height = 512;
   const int n_samples = 100;
   const int n_photons = 1000000;
   const int n_density_estimation = 100;
-  const bool final_gathering = false;
+  const bool final_gathering = true;
   const int max_depth = 100;
   const Vec3 camPos(2.78, 2.73, -9);
   const Vec3 lookAt(2.78, 2.73, 2.796);
@@ -121,7 +182,7 @@ int main() {
   const Camera camera(camPos, normalize(lookAt - camPos), 0.25 * PI);
 
   Scene scene;
-  cornellboxMirrorScene(scene);
+  cornellboxGlassScene(scene);
   scene.build();
 
   // photon tracing and build photon map
--- tests/test_path_tracing.cpp
+++ tests/test_path_tracing.cpp
@@ -105,6 +105,67 @@ void cornellboxMirrorScene(Scene& scene) {
   scene.addPrimitive(Primitive(light_shape, white, light));
 }
 
+void cornellboxGlassScene(Scene& scene) {
+  const auto white = std::make_shared<Lambert>(Vec3(0.8));
+  const auto red = std::make_shared<Lambert>(Vec3(0.8, 0.05, 0.05));
+  const auto green = std::make_shared<Lambert>(Vec3(0.05, 0.8, 0.05));
+  const auto glass = std::make_shared<Glass>(Vec3(0.99), 1.5f);
+
+  const auto floor =
+      std::make_shared<Plane>(Vec3(0), Vec3(0, 0, 5.592), Vec3(5.56, 0, 0));
+  const auto rightWall =
+      std::make_shared<Plane>(Vec3(0), Vec3(0, 5.488, 0), Vec3(0, 0, 5.592));
+  const auto leftWall = std::make_shared<Plane>(
+      Vec3(5.56, 0, 0), Vec3(0, 0, 5.592), Vec3(0, 5.488, 0));
+  const auto ceil = std::make_shared<Plane>(Vec3(0, 5.488, 0), Vec3(5.56, 0, 0),
+                                            Vec3(0, 0, 5.592));
+  const auto backWall = std::make_shared<Plane>(
+      Vec3(0, 0, 5.592), Vec3(0, 5.488, 0), Vec3(5.56, 0, 0));
+
+  const auto shortBox1 = std::make_shared<Plane>(
+      Vec3(1.3, 1.65, 0.65), Vec3(-0.48, 0, 1.6), Vec3(1.6, 0, 0.49));
+  const auto shortBox2 = std::make_shared<Plane>(
+      Vec3(2.9, 0, 1.14), Vec3(0, 1.65, 0), Vec3(-0.5, 0, 1.58));
+  const auto shortBox3 = std::make_shared<Plane>(
+      Vec3(1.3, 0, 0.65), Vec3(0, 1.65, 0), Vec3(1.6, 0, 0.49));
+  const auto shortBox4 = std::make_shared<Plane>(
+      Vec3(0.82, 0, 2.25), Vec3(0, 1.65, 0), Vec3(0.48, 0, -1.6));
+  const auto shortBox5 = std::make_shared<Plane>(
+      Vec3(2.4, 0, 2.72), Vec3(0, 1.65, 0), Vec3(-1.58, 0, -0.47));
+
+  const auto tallBox1 = std::make_shared<Plane>(
+      Vec3(4.23, 3.30, 2.47), Vec3(-1.58, 0, 0.49), Vec3(0.49, 0, 1.59));
+  const auto tallBox2 = std::make_shared<Plane>(
+      Vec3(4.23, 0, 2.47), Vec3(0, 3.3, 0), Vec3(0.49, 0, 1.59));
+  const auto tallBox3 = std::make_shared<Plane>(
+      Vec3(4.72, 0, 4.06), Vec3(0, 3.3, 0), Vec3(-1.58, 0, 0.5));
+  const auto tallBox4 = std::make_shared<Plane>(
+      Vec3(3.14, 0, 4.56), Vec3(0, 3.3, 0), Vec3(-0.49, 0, -1.6));
+  const auto tallBox5 = std::make_shared<Plane>(
+      Vec3(2.65, 0, 2.96), Vec3(0, 3.3, 0), Vec3(1.58, 0, -0.49));
+
+  const auto light_shape = std::make_shared<Plane>(
+      Vec3(3.43, 5.486, 2.27), Vec3(0, 0, 1.05), Vec3(-1.3, 0, 0));
+  const auto light = std::make_shared<AreaLight>(Vec3(34, 19, 10), light_shape);
+
+  scene.addPrimitive(Primitive(floor, white));
+  scene.addPrimitive(Primitive(rightWall, red));
+  scene.addPrimitive(Primitive(leftWall, green));
+  scene.addPrimitive(Primitive(ceil, white));
+  scene.addPrimitive(Primitive(backWall, white));
+  scene.addPrimitive(Primitive(shortBox1, glass));
+  scene.addPrimitive(Primitive(shortBox2, glass));
+  scene.addPrimitive(Primitive(shortBox3, glass));
+  scene.addPrimitive(Primitive(shortBox4, glass));
+  scene.addPrimitive(Primitive(shortBox5, glass));
+  scene.addPrimitive(Primitive(tallBox1, glass));
+  scene.addPrimitive(Primitive(tallBox2, glass));
+  scene.addPrimitive(Primitive(tallBox3, glass));
+  scene.addPrimitive(Primitive(tallBox4, glass));
+  scene.addPrimitive(Primitive(tallBox5, glass));
+  scene.addPrimitive(Primitive(light_shape, white, light));
+}
+
 int main() {
   const int width = 512;
   const int height = 512;
@@ -118,7 +179,7 @@ int main() {
   const Camera camera(camPos, normalize(lookAt - camPos), 0.25 * PI);
 
   Scene scene;
-  cornellboxMirrorScene(scene);
+  cornellboxGlassScene(scene);
   scene.build();
 
   // photon tracing and build photon map
@@ -137,6 +198,13 @@ int main() {
         float pdf;
         if (camera.sampleRay(Vec2(u, v), ray, pdf)) {
           const Vec3 radiance = integrator.integrate(ray, scene, sampler) / pdf;
+
+          if (std::isnan(radiance[0]) || std::isnan(radiance[1]) ||
+              std::isnan(radiance[2])) {
+            spdlog::error("radiance is NaN");
+            continue;
+          }
+
           image.addPixel(i, j, radiance);
         } else {
           image.setPixel(i, j, Vec3(0));
